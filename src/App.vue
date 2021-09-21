e6eec495d1e7ed6f1a5ab4dcd4a884926aca569a
Luis Bispo <luisguilhermebbispo@hotmail.com.br>
2021-09-20 23:27:56 -0300

    Estilizando Componente & Conceito Slot

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
+
+
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <h2 v-text="title"></h2>
-    <ul>
-      <li v-for="foto of fotos">
-        <img v-bind:src="foto.url" :alt="foto.title">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <!-- <h2 v-text="titulo"></h2> -->
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" v-bind:src="foto.url" v-bind:alt="foto.titulo">
+            <!-- <img :src="foto.url" :alt="foto.titulo"> -->
+        </meu-painel>
+
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+  components: {
+    'meu-painel' : Painel
+  },
+
   data() {
     return {
-      title: 'AluraPic',
+      titulo: 'AluraPic',
       fotos: [ ]
     }
   },
@@ -36,4 +52,23 @@ export default {
 
 <style lang="scss">
 
+  .corpo {
+      font-family: Helvetica, sans-serif;
+      width: 96%;
+      margin: 0 auto;
+  }
+  .centralizado{
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none;
+  }
+  .lista-fotos .lista-fotos-item{
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
